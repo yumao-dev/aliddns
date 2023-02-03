42225699473995bbf81c557e33c2cbd756be77d5
Diego <493223447@qq.com>
2023-02-03 18:29:50 +0800

    升级helper

--- app.ts
+++ app.ts
@@ -1,52 +1,63 @@
-import { createServer } from 'http';
-import { parse as queryParse } from 'querystring';
-import { parse } from 'url';
-import { DDNS } from './ddns';
-import { Logtype } from './helper/log/ilog';
-import { LogFactory } from './helper/log/logfactory';
-import { QueryParam } from './interface/settings';
-const log = LogFactory.create(Logtype.http);
+import { LogHelper } from "helper";
+import { createServer } from "http";
+import { parse as queryParse } from "querystring";
+import { parse } from "url";
+import { DDNS } from "./ddns";
+import { QueryParam } from "./interface/settings";
+const log = LogHelper.create();
 
 var server = createServer((req, res) => {
-    let reqaddress: string | undefined;
-    new Promise<QueryParam>((resolve, reject) => {
-        try {
-            reqaddress = (typeof req.headers['x-real-ip'] === 'string' ? req.headers['x-real-ip'] as string : undefined) || req.socket.remoteAddress;
-            let url = parse(req.url || '');
-            let params = queryParse(url.query || '');
+  let reqaddress: string | undefined;
+  new Promise<QueryParam>((resolve, reject) => {
+    try {
+      reqaddress =
+        (typeof req.headers["x-real-ip"] === "string"
+          ? (req.headers["x-real-ip"] as string)
+          : undefined) || req.socket.remoteAddress;
+      let url = parse(req.url || "");
+      let params = queryParse(url.query || "");
 
-            //参数格式化小写
-            let obj: { [key: string]: string | undefined; } = {};
-            for (const key in params) {
-                if (params[key]) {
-                    obj[key.toLowerCase()] = typeof params[key] === 'string' ? params[key] as string : undefined;
-                }
-            }
-
-            resolve({
-                apiKey: obj.apikey,
-                apiSecret: obj.apisecret,
-                domainName: obj.domainname,
-                ip: obj.ip || reqaddress
-            } as QueryParam);
-        } catch (error) {
-            reject(error);
+      //参数格式化小写
+      let obj: { [key: string]: string | undefined } = {};
+      for (const key in params) {
+        if (params[key]) {
+          obj[key.toLowerCase()] =
+            typeof params[key] === "string"
+              ? (params[key] as string)
+              : undefined;
         }
-    }).then(result => {
-        return new DDNS(reqaddress).update(result);
-    }).then(result => {
-        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-        res.end(JSON.stringify(result));
-    }).catch((err: Error) => {
-        // if (err.message != '参数有误') {
-        log.write(err, "Error", "更改DNS解析记录", reqaddress);
-        // }
-        res.writeHead(503, { 'Content-Type': 'application/json; charset=utf-8' });
-        res.end(JSON.stringify(err.message));
-    });
+      }
 
+      resolve({
+        apiKey: obj.apikey,
+        apiSecret: obj.apisecret,
+        domainName: obj.domainname,
+        ip: obj.ip || reqaddress,
+      } as QueryParam);
+    } catch (error) {
+      reject(error);
+    }
+  })
+    .then((result) => {
+      return new DDNS(reqaddress).update(result);
+    })
+    .then((result) => {
+      res.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+      res.end(JSON.stringify(result));
+    })
+    .catch((err: Error) => {
+      // if (err.message != '参数有误') {
+      log.write(err, "Error", "更改DNS解析记录", reqaddress);
+      // }
+      res.writeHead(503, { "Content-Type": "application/json; charset=utf-8" });
+      res.end(JSON.stringify(err.message));
+    });
 }).listen(process.env.PORT || 3000, () => {
-    let addr = server.address();
-    let bind = addr ? typeof addr === 'string' ? addr : `port:${addr.port}` : "UnKnow";
-    console.log(`server start on ${bind}`);
-})
+  let addr = server.address();
+  let bind = addr
+    ? typeof addr === "string"
+      ? addr
+      : `port:${addr.port}`
+    : "UnKnow";
+  console.log(`server start on ${bind}`);
+});
--- ddns.ts
+++ ddns.ts
@@ -1,140 +1,242 @@
 import Core from "@alicloud/pop-core";
-import { LogFactory, Logtype } from "./helper/log/logfactory";
-import { AliyunResponse, DescribeDomainRecordsRsp, QueryParam } from "./interface";
+import { LogHelper } from "helper/helper/loghelper";
+import { Address4, Address6 } from "ip-address";
+import {
+  AliyunResponse,
+  DescribeDomainRecordsRsp,
+  QueryParam,
+} from "./interface";
 import { Validate } from "./validate";
-import { Address4, Address6, } from "ip-address";
 
-
-const log = LogFactory.create(Logtype.http);
+const log = LogHelper.create();
 
 export class DDNS {
-    constructor(private reqip: string | undefined) {
+  constructor(private reqip: string | undefined) {}
+
+  public async update(param: QueryParam) {
+    //判断参数（存在以及各式）;
+
+    let valid = await new Validate().validate(param);
+
+    //更新域名记录
+    if (Address4.isValid(param.ip)) {
+      return await this.updatev4(param);
+    } else if (Address6.isValid(param.ip)) {
+      return await this.updatev6(param);
+    } else {
+      throw new Error("只支持v4或者v6");
     }
+  }
 
-    public async update(param: QueryParam) {
-        //判断参数（存在以及各式）;
+  private async updatev4(param: QueryParam) {
+    let domain = await new Validate().formatDomain(param.domainName);
+    const RRID = await this.DescribeDomainRecords(
+      param.apiKey,
+      param.apiSecret,
+      domain.domain,
+      domain.RR
+    );
+    let result = true;
+    if (RRID.v4) {
+      //有v4的解析记录存在
+      if (RRID.v4.value === param.ip) {
+        log.write(
+          `域名:${param.domainName},IPv4地址为：${param.ip}`,
+          "INFO",
+          "不需要修改解析记录",
+          this.reqip
+        );
+      } else {
+        result = await this.UpdateDomainRecord(
+          param.apiKey,
+          param.apiSecret,
+          RRID.v4.rid,
+          domain.RR,
+          "A",
+          param.ip
+        );
 
-        let valid = await new Validate().validate(param);
+        log.write(
+          `域名:${param.domainName},IPv4地址为：${param.ip},結果:${result}`,
+          "Warring",
+          "修改解析记录",
+          this.reqip
+        );
+      }
+    } else {
+      //没有v4的解析记录存在
+      result = await this.AddDomainRecord(
+        param.apiKey,
+        param.apiSecret,
+        domain.domain,
+        domain.RR,
+        "A",
+        param.ip
+      );
 
-        //更新域名记录
-        if (new Address4(param.ip).isValid()) {
-            return await this.updatev4(param)
-        } else if (new Address6(param.ip).isValid()) {
-            return await this.updatev6(param)
-        } else {
-            throw new Error('只支持v4或者v6');
-        }
+      log.write(
+        `域名:${param.domainName},IPv4地址为：${param.ip},結果:${result}`,
+        "Warring",
+        "添加解析记录",
+        this.reqip
+      );
     }
+    return result;
+  }
 
-    private async updatev4(param: QueryParam) {
-        let domain = await new Validate().formatDomain(param.domainName);
-        const RRID = await this.DescribeDomainRecords(param.apiKey, param.apiSecret, domain.domain, domain.RR);
-        let result = true;
-        if (RRID.v4) {
-            //有v4的解析记录存在
-            if (RRID.v4.value === param.ip) {
-                log.write(`域名:${param.domainName},IPv4地址为：${param.ip}`, "INFO", "不需要修改解析记录", this.reqip);
-            } else {
-                result = await this.UpdateDomainRecord(param.apiKey, param.apiSecret, RRID.v4.rid, domain.RR, "A", param.ip);
+  private async updatev6(param: QueryParam) {
+    let domain = await new Validate().formatDomain(param.domainName);
+    const RRID = await this.DescribeDomainRecords(
+      param.apiKey,
+      param.apiSecret,
+      domain.domain,
+      domain.RR
+    );
+    let result = true;
+    if (RRID.v6) {
+      if (RRID.v6.value === param.ip) {
+        log.write(
+          `域名:${param.domainName},IPv6地址为：${param.ip}`,
+          "INFO",
+          "不需要修改解析记录",
+          this.reqip
+        );
+      } else {
+        result = await this.UpdateDomainRecord(
+          param.apiKey,
+          param.apiSecret,
+          RRID.v6.rid,
+          domain.RR,
+          "AAAA",
+          param.ip
+        );
 
-                log.write(`域名:${param.domainName},IPv4地址为：${param.ip},結果:${result}`, "Warring", "修改解析记录", this.reqip);
-            }
-        } else {
-            //没有v4的解析记录存在
-            result = await this.AddDomainRecord(param.apiKey, param.apiSecret, domain.domain, domain.RR, "A", param.ip);
-
-            log.write(`域名:${param.domainName},IPv4地址为：${param.ip},結果:${result}`, "Warring", "添加解析记录", this.reqip);
-        }
-        return result;
+        log.write(
+          `域名:${param.domainName},IPv6地址为：${param.ip},結果:${result}`,
+          "Warring",
+          "修改解析记录",
+          this.reqip
+        );
+      }
+    } else {
+      //没有v4的解析记录存在
+      result = await this.AddDomainRecord(
+        param.apiKey,
+        param.apiSecret,
+        domain.domain,
+        domain.RR,
+        "AAAA",
+        param.ip
+      );
+      log.write(
+        `域名:${param.domainName},IPv6地址为：${param.ip},結果:${result}`,
+        "Warring",
+        "添加解析记录",
+        this.reqip
+      );
     }
+    return result;
+  }
 
-    private async updatev6(param: QueryParam) {
-        let domain = await new Validate().formatDomain(param.domainName);
-        const RRID = await this.DescribeDomainRecords(param.apiKey, param.apiSecret, domain.domain, domain.RR);
-        let result = true;
-        if (RRID.v6) {
-            if (RRID.v6.value === param.ip) {
-                log.write(`域名:${param.domainName},IPv6地址为：${param.ip}`, "INFO", "不需要修改解析记录", this.reqip);
-            } else {
-                result = await this.UpdateDomainRecord(param.apiKey, param.apiSecret, RRID.v6.rid, domain.RR, "AAAA", param.ip);
+  private async DescribeDomainRecords(
+    apiKey: string,
+    apiSecret: string,
+    domainName: string,
+    RR: string
+  ): Promise<DescribeDomainRecordsRsp> {
+    const result: AliyunResponse.DNS.DescribeDomainRecords = await new Core({
+      accessKeyId: apiKey,
+      accessKeySecret: apiSecret,
+      endpoint: "https://alidns.aliyuncs.com",
+      apiVersion: "2015-01-09",
+    }).request(
+      "DescribeDomainRecords",
+      { DomainName: domainName },
+      { method: "POST" }
+    );
 
-                log.write(`域名:${param.domainName},IPv6地址为：${param.ip},結果:${result}`, "Warring", "修改解析记录", this.reqip);
-            }
-        } else {
-            //没有v4的解析记录存在
-            result = await this.AddDomainRecord(param.apiKey, param.apiSecret, domain.domain, domain.RR, "AAAA", param.ip);
-            log.write(`域名:${param.domainName},IPv6地址为：${param.ip},結果:${result}`, "Warring", "添加解析记录", this.reqip);
-        }
-        return result;
+    const RRExists4 = result.DomainRecords.Record.findIndex((v) => {
+      if (v.RR === RR && v.Type === "A") return true;
+      return false;
+    });
+
+    const RRExists6 = result.DomainRecords.Record.findIndex((v) => {
+      if (v.RR === RR && v.Type === "AAAA") return true;
+      return false;
+    });
+
+    return {
+      v4:
+        RRExists4 < 0
+          ? false
+          : {
+              rid: result.DomainRecords.Record[RRExists4].RecordId,
+              value: result.DomainRecords.Record[RRExists4].Value,
+            },
+      v6:
+        RRExists6 < 0
+          ? false
+          : {
+              rid: result.DomainRecords.Record[RRExists6].RecordId,
+              value: result.DomainRecords.Record[RRExists6].Value,
+            },
+    };
+  }
+
+  private async AddDomainRecord(
+    apiKey: string,
+    apiSecret: string,
+    DomainName: string,
+    RR: string,
+    Type: "A" | "AAAA",
+    Value: string,
+    TTL: number = 600
+  ) {
+    try {
+      const result: AliyunResponse.DNS.AddDomainRecord = await new Core({
+        accessKeyId: apiKey,
+        accessKeySecret: apiSecret,
+        endpoint: "https://alidns.aliyuncs.com",
+        apiVersion: "2015-01-09",
+      }).request(
+        "AddDomainRecord",
+        { DomainName, RR, Type, Value, TTL },
+        { method: "POST" }
+      );
+
+      return true;
+    } catch (error) {
+      console.log(error);
+
+      return false;
     }
+  }
 
+  private async UpdateDomainRecord(
+    apiKey: string,
+    apiSecret: string,
+    RecordId: string,
+    RR: string,
+    Type: "A" | "AAAA",
+    Value: string,
+    TTL: number = 600
+  ) {
+    try {
+      const result: AliyunResponse.DNS.UpdateDomainRecord = await new Core({
+        accessKeyId: apiKey,
+        accessKeySecret: apiSecret,
+        endpoint: "https://alidns.aliyuncs.com",
+        apiVersion: "2015-01-09",
+      }).request(
+        "UpdateDomainRecord",
+        { RecordId, RR, Type, Value, TTL },
+        { method: "POST" }
+      );
 
-    private async DescribeDomainRecords(apiKey: string, apiSecret: string, domainName: string, RR: string): Promise<DescribeDomainRecordsRsp> {
-        const result: AliyunResponse.DNS.DescribeDomainRecords = await new Core({
-            accessKeyId: apiKey,
-            accessKeySecret: apiSecret,
-            endpoint: 'https://alidns.aliyuncs.com',
-            apiVersion: '2015-01-09'
-        }).request('DescribeDomainRecords', { "DomainName": domainName }, { method: 'POST' });
-
-        const RRExists4 = result.DomainRecords.Record.findIndex(v => {
-            if (v.RR === RR && v.Type === 'A') return true;
-            return false;
-        });
-
-        const RRExists6 = result.DomainRecords.Record.findIndex(v => {
-            if (v.RR === RR && v.Type === 'AAAA') return true;
-            return false;
-        });
-
-        return {
-            v4: RRExists4 < 0 ? false : { rid: result.DomainRecords.Record[RRExists4].RecordId, value: result.DomainRecords.Record[RRExists4].Value },
-            v6: RRExists6 < 0 ? false : { rid: result.DomainRecords.Record[RRExists6].RecordId, value: result.DomainRecords.Record[RRExists6].Value }
-        }
-
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-
-    private async  AddDomainRecord(apiKey: string, apiSecret: string, DomainName: string, RR: string, Type: 'A' | 'AAAA', Value: string, TTL: number = 600) {
-        try {
-
-            const result: AliyunResponse.DNS.AddDomainRecord = await new Core({
-                accessKeyId: apiKey,
-                accessKeySecret: apiSecret,
-                endpoint: 'https://alidns.aliyuncs.com',
-                apiVersion: '2015-01-09'
-            }).request('AddDomainRecord', { DomainName, RR, Type, Value, TTL }, { method: 'POST' });
-
-            return true;
-
-        } catch (error) {
-
-            console.log(error);
-
-            return false;
-
-        }
-
-    }
-
-    private async  UpdateDomainRecord(apiKey: string, apiSecret: string, RecordId: string, RR: string, Type: 'A' | 'AAAA', Value: string, TTL: number = 600) {
-
-        try {
-            const result: AliyunResponse.DNS.UpdateDomainRecord = await new Core({
-                accessKeyId: apiKey,
-                accessKeySecret: apiSecret,
-                endpoint: 'https://alidns.aliyuncs.com',
-                apiVersion: '2015-01-09'
-            }).request('UpdateDomainRecord', { RecordId, RR, Type, Value, TTL }, { method: 'POST' });
-
-            return true;
-
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-
-    }
-
-
-
+  }
 }
